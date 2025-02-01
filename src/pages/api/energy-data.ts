d4a83ec2337c6036b273babeed1b4502ee2c117b
Sujoy <sujoy.k.mondol@gmail.com>
2025-02-01 18:18:47 -0500

    DB Authentication Fixed

--- src/pages/api/energy-data.ts
+++ src/pages/api/energy-data.ts
@@ -2,16 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import EnergyData from '../../models/EnergyData';
 import dbConnect from '../../lib/dbConnect';
 
+
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
   await dbConnect();
 
   if (req.method === 'POST') {
     try {
+      console.log('✅ Connected to MongoDB');
+      console.log('📝 Request body:', req.body);
       const newData = await EnergyData.create(req.body);
+      console.log('💾 Saved data:', newData);
       res.status(201).json(newData);
+
     } catch  {
       res.status(500).json({ error: 'Failed to save data' });
     }
